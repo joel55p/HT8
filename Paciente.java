922ec53a43f8055c3bdc5694e012abdc363f67a0
joel55p <joelnerio42@gmail.com>
2025-03-27 12:02:14 -0600

    se implemento interfaz comparable para poder hacer un compareTo para el codigo

--- Paciente.java
+++ Paciente.java
@@ -1,23 +1,40 @@
-public class Paciente {
-    private String nombre;
-    private String enfermedad;
-    private String codigo;
+public class Paciente implements  Comparable<Paciente> { /*tiene que tener la interfaz Comparable  */
+    private String nombre; /*nombre del paciente */
+    private String enfermedad; /*info extra */
+    private char codigo; /*Esto es lo mas importante y va a ser de tipo caracter  */
 
-    public Paciente(String nombre, String enfermedad, String Codigo) {
+    public Paciente(String nombre, String enfermedad, char Codigo) { /*Constructor */
         this.nombre = nombre;
-        this.codigo = Codigo;
         this.enfermedad = enfermedad;
+        this.codigo = Codigo;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public String getCodigo() {
+    public char getCodigo() { /*importante */
         return codigo;
     }
 
     public String getEnfermedad() {
         return enfermedad;
+    }
+
+
+    @Override
+
+    public int  compareTo(Paciente o) { /*para comparar los objetos */
+
+        return Character.compare(this.codigo, o.codigo);
+    }
+
+    @Override
+    public String toString() { /*para imprimir el objeto, el toString*/
+        return "Paciente{" +
+                "nombre='" + nombre + '\'' +
+                ", enfermedad='" + enfermedad + '\'' +
+                ", codigo=" + codigo +
+                '}';
     }
 }
